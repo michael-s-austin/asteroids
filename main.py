861484b3f7aded7f8dcc7161835cf5f6ca78d08a
Michael Austin <michaelaustin78@gmail.com>
2026-04-29 16:55:18 -0500

    Initialize clock and delta time for frame rate control

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print("Starting Asteroids with pygame version: VERSION")
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
