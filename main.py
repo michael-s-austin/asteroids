b2e16521e5329e20313b3edac2b75159c2c37bdb
Michael Austin <michaelaustin78@gmail.com>
2026-04-30 21:49:04 -0500

    Add player class and circle shape base class; update main function to include player initialization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame   
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,9 +9,13 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -18,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
